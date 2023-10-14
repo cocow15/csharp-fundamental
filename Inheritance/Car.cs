5c1e13e151eacb89c0ad8ea5d800167ed39a3da0
Dimas Wisnu Saputro <Dimaswisnu2500@gmail.com>
2023-10-14 09:33:30 +0700

    Feat(Inheritance): Add Example

---
+++ Inheritance/Car.cs
@@ -0,0 +1,10 @@
+Car.cs
+using System;
+
+namespace MyApplication
+{
+  class Car : Vehicle  // Derived class
+  {
+    public string modelName = "Mustang";  // Car field
+  }
+}
---
+++ Inheritance/Vehicle.cs
@@ -0,0 +1,14 @@
+Vehicle.cs
+using System;
+
+namespace MyApplication
+{
+  class Vehicle  // Base class
+  {
+    public string brand = "Ford";  // Vehicle field
+    public void honk()             // Vehicle method 
+    {
+      Console.WriteLine("Tuut, tuut!");
+    }
+  }
+}
